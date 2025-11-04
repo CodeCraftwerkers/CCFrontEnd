80ac5f85d6117dcc9c12d98bec8d65b2b59dcb7c
DevErika <erikamontoya2023@gmail.com>
2025-11-04 09:21:19 +0100

    fix(navbar):linked navbar to homepage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav className="w-screen fixed top-0 left-0 bg-white shadow-md z-9999">
+    <nav className="w-screen fixed top-0 left-0 bg-white shadow-md z-50">
       <div className="flex items-center justify-between px-6 md:px-10 py-4 h-[70px]">
-        <img
-          src="src\assets\img\LogoGradient_NaranjaButano.png"
-          alt="Logo"
-          className="h-10 md:h-20 object-contain"
-        />
-        <div className="flex items-center gap-3 md:gap-5">
+        <Link to="/" className="flex items-center gap-2">
+          <img
+            src="/src/assets/img/LogoGradient_NaranjaButano.png"
+            alt="Code Crafters Logo"
+            className="h-10 md:h-20 object-contain"
+          />
+        </Link>
+         <div className="flex items-center gap-3 md:gap-5">
           <button className="text-gray-800 text-sm md:text-base hover:text-purple-700 transition-colors">
             Iniciar sesión
           </button>
